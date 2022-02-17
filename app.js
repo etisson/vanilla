eb5cb136ff6529b1614e87db90be9ed88602c3fa
etisson <etisson@protonmail.com>
2022-02-17 11:07:57 +0100

    addedn entities

--- app.js
+++ app.js
@@ -8,9 +8,27 @@ const rl = Readline.createInterface({
     terminal: false
 });
 
+const matcher = require('./matcher');
+
 rl.setPrompt('> ');
 rl.prompt();
+
 rl.on('line', reply => {
-    console.log(`You said: ${reply}`);
-    rl.prompt();
-})
+    matcher(reply, data => {
+        switch(data.intent) {
+            case 'Hello':
+                console.log(`${data.entities.greeting} to you too!`);
+                rl.prompt();
+                break;
+            case 'Exit':
+                console.log("See you next time");
+                process.exit(0);
+                break;
+            default: {
+                console.log("I don't know what you mean! ");
+                rl.prompt();
+                break;
+            }
+        }
+    });
+});
